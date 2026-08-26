645797189d5d4f14ab6a8187accc5f0ef99a4dad
WhosArjun <arjun.arunkumar.v@gmail.com>
2026-08-26 15:24:57 -0400

    add herons formula to worksheet

--- Interface/InterfaceShapeRectangle.java
+++ Interface/InterfaceShapeRectangle.java
@@ -4,7 +4,7 @@ public class InterfaceShapeRectangle {
     public static void main(String[] args){
         Shape rectangle = new Rectangle(6,7);
         Shape circle = new Circle(6.7);
-        Shape triangle = new Triangle(6, 7);
+        Shape triangle = new Triangle(3,4,2);
 
         System.out.println(rectangle.area());
         System.out.println(triangle.area());
@@ -32,16 +32,20 @@ class Rectangle implements Shape{
 
 
 class Triangle implements Shape{
-    private double base;
-    private double height;
-
-    public Triangle(double base, double height){
-        this.base = base;
-        this.height = height;
+    private double s1;
+    private double s2;
+    private double s3;
+    private double s;
+    
+    public Triangle(double s1, double s2, double s3){
+        this.s1 = s1;
+        this.s2 = s2;
+        this.s3 = s3;
+        this.s = (s1+s2+s3)/2.0;
     }
 
     public double area(){
-        return (base*height)/2.0;
+        return Math.sqrt(s*(s-1)*(s-2)*(s-3));
     }
 }
 
